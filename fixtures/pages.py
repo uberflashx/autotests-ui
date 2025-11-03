d6879f353b0cf28a0667536445312c659bad3b56
uberflashx <uberflashx@gmail.com>
2025-11-03 19:16:22 +0300

    Практикуемся в реализации PageObject

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,6 +1,9 @@
 import pytest
 from playwright.sync_api import Page
 
+from fixtures.browsers import chromium_page_with_state
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
 from pages.registration_page import RegistrationPage
@@ -17,3 +20,11 @@ def registration_page(chromium_page: Page) -> RegistrationPage:
 @pytest.fixture
 def dashboard_page(chromium_page: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page)
+
+@pytest.fixture
+def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page_with_state)
+
+
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page_with_state)
