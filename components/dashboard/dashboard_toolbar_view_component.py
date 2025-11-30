867cf2a964fed436ffe86ad65d6ceb603cf2b93b
uberflashx <uberflashx@gmail.com>
2025-11-30 23:30:20 +0300

    Знакомство с PageFactory. Практика работы с Page Factory

--- components/dashboard/dashboard_toolbar_view_component.py
+++ components/dashboard/dashboard_toolbar_view_component.py
@@ -1,12 +1,13 @@
 from components.base_component import BaseComponent
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
+from elements.text import Text
 
 class DashboardToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.title = page.get_by_test_id('dashboard-toolbar-title-text')
+        self.title = Text(page, 'dashboard-toolbar-title-text', 'Dashboard title')
 
     def check_visible(self):
-        expect(self.title).to_be_visible()
-        expect(self.title).to_have_text('Dashboard')
+        self.title.check_visible()
+        self.title.check_have_text('Dashboard')
--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,6 +1,8 @@
 from components.authentication.registration_form_component import RegistrationFormComponent
 from pages.base_page import BasePage
-from playwright.sync_api import expect, Page
+from playwright.sync_api import Page
+from elements.link import Link
+from elements.button import Button
 
 class RegistrationPage(BasePage):
     def __init__(self, page: Page):
@@ -8,8 +10,8 @@ class RegistrationPage(BasePage):
 
         self.registration_form = RegistrationFormComponent(page)
 
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
-        self.login_link = page.get_by_test_id('registration-page-login-link')
+        self.registration_button = Button(page, 'registration-page-registration-button', 'Registration')
+        self.login_link = Link(page, 'registration-page-login-link', 'Login')
         self.user_already_exists_alert = page.get_by_test_id('registration-page-user-already-exists-alert')
 
     def click_registration_button(self):
