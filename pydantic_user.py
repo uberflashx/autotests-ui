29706430fa50105c8b412afb8ce32f25bb5bfda5
uberflashx <uberflashx@gmail.com>
2025-12-23 20:25:39 +0300

    Настройки автотестов с Pydantic

---
+++ pydantic_user.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    is_active: bool = True
+
+
+user_data = {
+    "id": 1,
+    "username": "Zara",
+    "email": "zara.bond@gmail.com"
+}
+
+user = User(**user_data)
+print(user)
+print(user.is_active)
+
+invalid_user_data = {
+    "id": "one",
+    "username": "Zara",
+    "email": "zara.bond@gmail.com"
+}
+
+try:
+    invalid_user = User(**invalid_user_data)
+except Exception as error:
+    print('Ошибка валидации:', error)
